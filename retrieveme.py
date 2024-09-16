f2ad7eaab40c137ac3d7a803d5addb42c244914f
John Heibel <jhcat99@gmail.com>
2024-09-15 18:37:23 -0700

    Update retrieveme.py

--- retrieveme.py
+++ retrieveme.py
@@ -1,13 +1,5 @@
-SOLUTION TO THE FOLLOWING GITHUB ISSUE:
+the be to of and a in that have I it for not on with he as you do at this but his by from they we say her she or an will my one all would there their what so up out if about who get which go me when make can like time no just him know take people into year your good some could them see other than then now look only come its over think also back after use two how our work first well way even new want because any these give day most us Github Github Github
 
-Set default FILE_UPLOAD_PERMISSION to 0o644.
-Description
-	
-Hello,
-As far as I can see, the ​File Uploads documentation page does not mention any permission issues.
-What I would like to see is a warning that in absence of explicitly configured FILE_UPLOAD_PERMISSIONS, the permissions for a file uploaded to FileSystemStorage might not be consistent depending on whether a MemoryUploadedFile or a TemporaryUploadedFile was used for temporary storage of the uploaded data (which, with the default FILE_UPLOAD_HANDLERS, in turn depends on the uploaded data size).
-The tempfile.NamedTemporaryFile + os.rename sequence causes the resulting file permissions to be 0o0600 on some systems (I experience it here on CentOS 7.4.1708 and Python 3.6.5). In all probability, the implementation of Python's built-in tempfile module explicitly sets such permissions for temporary files due to security considerations.
-I found mentions of this issue ​on GitHub, but did not manage to find any existing bug report in Django's bug tracker.
 
 
 **SOLUTION:**
